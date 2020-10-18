8c9ef2b04eb5c9c59b6ad8fb715177f236f0984d
b.peng <pengbo9306@gmail.com>
2020-10-19 00:28:48 +0800

    le_net实现

--- leetcode/LikedList.py
+++ leetcode/LikedList.py
@@ -13,4 +13,27 @@ class Solution:
             hb = hb.next if ha else headA
         return ha
 
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        cur = head
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
 
+
+solution = Solution()
+head = ListNode(4)
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(1)
+head.next = node1
+node1.next = node2
+node2.next = node3
+result = solution.reverseList(head)
+
+while result:
+    print(result.val)
+    result = result.next
